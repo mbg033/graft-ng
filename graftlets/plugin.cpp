e478a7c3129a44f371d60231ba5b30f3994cd2ea
Alexander Suprunenko <alexander.suprunenko@graft.network>
2018-08-01 00:07:37 +0300

    ASIS

---
+++ graftlets/plugin.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "CMakePluginConfig.h"
+//#include <boost/config.hpp> // for BOOST_SYMBOL_EXPORT
+#include <boost/dll.hpp>
+#include <boost/dll/alias.hpp> // for BOOST_DLL_ALIAS
+
+#define API extern "C" BOOST_SYMBOL_EXPORT
+
+namespace graft
+{
+
+API void my_function_x() noexcept
+{
+    std::cout << "\nthe version " << PROJECT_VERSION_MAJOR << '.' << PROJECT_VERSION_MINOR << "\n";
+}
+
+} //namespace graft
+
+BOOST_DLL_ALIAS(graft::my_function_x, my_function);
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,57 @@
 #include "server.h"
 
+#include <boost/config.hpp> // for BOOST_SYMBOL_EXPORT
+#include <boost/dll/import.hpp> // for import_alias
+
+void test_graftlet(int argc, const char** argv)
+{
+    boost::filesystem::path lib_dir(argv[0]);
+    lib_dir.remove_filename();
+
+    auto my_function_x = boost::dll::import<void()>(
+                lib_dir / "graftlets/libmy_graftlet", "my_function_x",
+                boost::dll::load_mode::append_decorations
+                );
+    my_function_x();
+
+    auto my_function = boost::dll::import_alias<void()>(
+                lib_dir / "graftlets/libmy_graftlet", "my_function",
+                boost::dll::load_mode::append_decorations
+                );
+    my_function();
+
+    try
+    {//invalid test
+        auto my_bla = boost::dll::import<void(std::string&)>(
+                    lib_dir / "graftlets/libmy_graftlet", "my_function_x",
+                    boost::dll::load_mode::append_decorations
+                    );
+        std::string s("sss");
+        my_bla(s);
+    }
+    catch(std::exception& ex)
+    {
+        std::cout << "\n exception:" << ex.what();
+    }
+
+    try
+    {
+        auto my_bla = boost::dll::import_alias<void(std::string&)>(
+                    lib_dir / "graftlets/libmy_graftlet", "xxx",
+                    boost::dll::load_mode::append_decorations
+                    );
+    }
+    catch(std::exception& ex)
+    {
+        std::cout << "\n exception:" << ex.what();
+    }
+}
+
 int main(int argc, const char** argv)
 {
+    test_graftlet(argc, argv);
+    return 0;
+
     try
     {
         graft::GraftServer gserver;
